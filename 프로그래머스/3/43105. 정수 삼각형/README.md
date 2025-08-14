32b11ee8820e1eaf4b20d1e0ecc1269d6c58fa39
justinchoibiz <justinchoi.official@gmail.com>
2025-08-14 21:39:58 +0900

    [level 3] Title: 정수 삼각형, Time: 39.64 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
 
 ### 성능 요약
 
-메모리: 14 MB, 시간: 52.19 ms
+메모리: 12.9 MB, 시간: 39.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 14일 09:34:56
+2025년 08월 14일 21:39:56
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,8 +1,8 @@
 def solution(triangle):
-    n = len(triangle)
-
+    n = len(triangle);
+    
     for i in range(n - 2, -1, -1):
-        for j in range(len(triangle[i])):
-            triangle[i][j] = triangle[i][j] + max(triangle[i + 1][j], triangle[i + 1][j + 1])
-
+        for j in range (i + 1):
+            triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+    
     return triangle[0][0]
