d86da701908100a9a2e3a7ef6837bec3c19451f9
justinchoibiz <justinchoi.official@gmail.com>
2025-08-14 21:02:07 +0900

    [level 3] Title: 네트워크, Time: 1.07 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -2,14 +2,14 @@ def solution(n, computers):
     answer = 0;
     visited = [False] * n;
     
-    def dfs(computer_idx):
-        visited[computer_idx] = True;
-        for neighbor_idx in range(n):
-            if computers[computer_idx][neighbor_idx] == 1 and not visited[neighbor_idx]:
-                dfs(neighbor_idx)
-    
-    for i in range(n):
+    def dfs(c_idx):
+        visited[c_idx] = True;
+        for n_idx in range(n):
+            if computers[c_idx][n_idx] == 1 and not visited[n_idx]:
+                dfs(n_idx);
+    for i in (range(n)):
         if not visited[i]:
             dfs(i);
-            answer += 1;    
+            answer += 1;
+            
     return answer;
